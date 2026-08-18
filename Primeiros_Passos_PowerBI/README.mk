c7f81f9a6cabb4c34e064efe5ff281ebf9708be5
LeandroYamanaka <leandroyamanaka@gmail.com>
2026-08-17 22:31:55 -0300

    First Commit

---
+++ Primeiros_Passos_PowerBI/README.mk
@@ -0,0 +1,22 @@
+# Desafio DIO - Primeiros Passos PowerBI
+
+## Visão Geral do Trabalho
+
+Neste projeto foram feitas réplicas de análises visando realizar os primeiros contatos com a ferramenta e explorar algumas funções, com a criação de gráficos, alteração de fontes e formatos e combinação de informações.  
+
+## Etapas do Projeto
+
+### 1. Fontes
+
+Foram utilizados datasets disponíveis no Kaggle e PowerBI Sample
+
+### 2. Imagens
+
+<img src="./Imagens/Diabetes.jpg" width="500" height="300">
+
+<img src="./Imagens/Financial.jpg" width="500" height="300">
+
+## Conclusões
+
+***Primeiro contato para familiarizar com a ferramenta, descobrir funcionalidades e possibilidades de uso das informações.
+
